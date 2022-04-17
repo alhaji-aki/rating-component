02abda610eb6bda0e176ec4c6dcc2741332bf097
Alhaji Aki <abdulkudus2922@gmail.com>
2022-04-17 20:58:54 +0000

    completed structure and button states

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediumGrey: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+    },
   },
   plugins: [],
 };
