23db88ccadd900da00db89080e6a08104770a4ca
Alhaji Aki <abdulkudus2922@gmail.com>
2022-04-18 08:18:31 +0000

    added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
     extend: {
+      fontFamily: {
+        sans: ["Overpass", "sans-serif"],
+      },
       colors: {
         orange: "hsl(25, 97%, 53%)",
         lightGrey: "hsl(217, 12%, 63%)",
